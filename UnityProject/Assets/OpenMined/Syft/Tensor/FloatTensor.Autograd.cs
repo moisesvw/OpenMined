c90f4ea78bc9a02c08c11b249c7f6222c94ac3e7
Andrew Trask <liamtrask@gmail.com>
2017-12-06 15:02:01 +0000

    added FloatTensor.autograd.cs

---
+++ UnityProject/Assets/OpenMined/Syft/Tensor/FloatTensor.Autograd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenMined.Syft.Tensor
+{
+	public partial class FloatTensor
+	{
+
+		private bool autograd;
+		private bool keepgrads;
+
+		private Dictionary<int, FloatTensor> creators;
+		private string creation_op;
+		private Dictionary<int, FloatTensor> children;
+
+		private FloatTensor grad;
+
+
+
+
+	}
+}
